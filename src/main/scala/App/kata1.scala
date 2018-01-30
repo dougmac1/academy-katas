8e5fca281f828db490dfd4fef672f54f4fff4bea
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-01-30 10:40:50 +0000

    No changes

--- src/main/scala/App/kata1.scala
+++ src/main/scala/App/kata1.scala
@@ -1,12 +1,13 @@
 package App
 
-import jdk.internal.util.xml.impl.Input
-
 import scala.io.StdIn
 
 object kata1 extends App  {
 
   val x = StdIn.readLine("Input a number \n")toInt
+
   def countdown (input: Int) = List.range(1, input +1).reverse
+
   println(countdown(x))
+
 }
--- src/main/scala/App/kata2.scala
+++ src/main/scala/App/kata2.scala
@@ -5,8 +5,9 @@ import scala.io.StdIn
 object kata2 extends App  {
 
   val input = StdIn.readLine("Enter a word \n")
+
   val num = StdIn.readLine("Enter a number \n").toInt
 
-  println((s"$input ") * num)
+  println(s"$input " * num)
 
 }
